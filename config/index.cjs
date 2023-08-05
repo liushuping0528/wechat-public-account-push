1388f074be1cd52be93f2628ef25c90b7004d7db
Eureka <51844464+liushuping0528@users.noreply.github.com>
2023-08-05 21:15:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owv7u6td9eFCsVno-Fhh8VWreUAM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1UR9eREkdB7YKuOnUvXsiFiw1zZH7ruLyU4XPVObHyU',
+      useTemplateId: 'v90s0F7aODXmE3gopqIoYgkhB9a8kSTWFE-o1coqcZw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [

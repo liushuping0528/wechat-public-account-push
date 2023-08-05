183621794d71559e5f244ea10dbada1b0673dc61
Eureka <51844464+liushuping0528@users.noreply.github.com>
2023-08-05 21:22:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,6 +21,53 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '张雪盼',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'owv7u6jf3oIFqP3PodHeiMRlBeQg',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'v90s0F7aODXmE3gopqIoYgkhB9a8kSTWFE-o1coqcZw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '张雪盼', year: '2001', date: '03-08',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '张雪盼', year: '2001', date: '04-16',
+        },
+        {
+          type: '节日', name: '第一次相识', year: '2023', date: '04-02',
+        },
+        {
+          type: '节日', name: '七夕', year: '2023', date: '08-22',
+        },
+        {
+          type: '节日', name: '中秋节', year: '2023', date: '09-29',
+        },
+        {
+          type: '节日', name: '国庆节', year: '2023', date: '10-01',
+        },
+        
+        {
+          type: '节日', name: '周年纪念日', year: '2023', date: '07-06',
+        },
+        {
+          type: '节日', name: '2024跨年', year: '2024', date: '01-01',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-07-06' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+
+    {
+      // 想要发送的人的名字
+      name: '刘书平',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owv7u6td9eFCsVno-Fhh8VWreUAM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'v90s0F7aODXmE3gopqIoYgkhB9a8kSTWFE-o1coqcZw',
